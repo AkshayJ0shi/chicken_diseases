dad8a6fa7d008c2ad91a8acc46f43c43325b436e
Akshay Joshi <akshayjoshi1117@gmail.com>
2023-06-05 12:54:50 +0530

    created project template

---
+++ template.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+project_name = "Chicken_Disease"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    # we need to install project as a local package
+    # hence we are using __init__.py here 
+    f"src{project_name}/__init__.py",
+    f"src{project_name}/components/__init__.py",
+    f"src{project_name}/utils/__init__.py",
+    f"src{project_name}/config/__init__.py",
+    f"src{project_name}/config/configuration/__init__.py",
+    f"src{project_name}/pipeline/__init__.py",
+    f"src{project_name}/entity/__init__.py",
+    f"src{project_name}/constants/__init__.py",
+    "config.config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory : {filedir} for the file : {filename}")
+
+    # if a file has no content, os.path.getsize(filepath) is equal to 0
+    # if something is present, it will output a non-zero value
+    # if there is comething present in the file, we dont want to override that
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file : {filepath}")
+
+    else:
+        logging.info(f"{filename} already exists..")
